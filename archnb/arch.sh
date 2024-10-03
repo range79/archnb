0376d50543f672ecafb9e1ca5916adc39b02fef9
range79 <nija4elements@gmail.com>
2024-10-04 03:34:59 +0400

    1.1.0 surumu

--- archnb/arch.sh
+++ archnb/arch.sh
@@ -14,7 +14,7 @@ _________________________________
 |         başlayanlar için      |
 |             script            | 
 |                               | 
-|           version 1.0.2       | 
+|           version 1.1.0       | 
 |       github: ${RED}@range79${RESET}        |
 |       telegram: ${RED}@Onlyrange${RESET}    |
 |                               |
@@ -53,47 +53,63 @@ fi
 
 echo "Sudo şifrenizi girin:"
 read -s SUDO_PASSWORD
-echo "$SUDO_PASSWORD" | sudo -S pacman -S fzf --noconfirm
-echo "$SUDO_PASSWORD" | sudo -S pacman -S flatpak -noconfirm
+
 echo "$SUDO_PASSWORD" | sudo -S true 2>/dev/null
 if [ $? -ne 0 ]; then
     echo "Geçersiz şifre!"
     exit 1
 fi
-
+echo "$SUDO_PASSWORD" | sudo -S pacman -S flatpak -noconfirm
 echo "$SUDO_PASSWORD" | sudo -S pacman -S fzf --noconfirm
 
+
+
+echo "Yay Paket Yöneticisi kurulumu başlayacak..."
+cd /home/$USER || { echo "Kullanıcı dizinine geçiş başarısız!"; exit 1; }
+echo "$SUDO_PASSWORD" | sudo -S git clone https://aur.archlinux.org/yay.git
+cd yay || { echo "Yay dizinine geçiş başarısız!"; exit 1; }
+echo "$SUDO_PASSWORD" | makepkg -si --noconfirm
+            
+                
+
 if [ "$NAME" = "Arch Linux" ]; then
-    SELECTION=$(echo -e "Yay\nAnydesk\nWhatsapp\nDiscord\n" | fzf --multi --prompt="Arch'inize ne yüklemek istersiniz? Bir veya daha fazla işlem seçin (Tab tuşuyla seçim yapılıyor): ")
+    SELECTION=$(echo -e "Anydesk\nWhatsapp\nDiscord\nModluDc\nVscode" | fzf --multi --prompt="Arch'inize ne yüklemek istersiniz? Bir veya daha fazla işlem seçin (Tab tuşuyla seçim yapılıyor): ")
 
     for OPTION in $SELECTION; do
         case $OPTION in
-            "Yay")
-                echo "Yay Paket Yöneticisi kurulumu başlayacak..."
-                whoami = benim
-            cd /home/${benim}
-        
-                echo "$SUDO_PASSWORD" | sudo -S git clone https://aur.archlinux.org/yay.git
-         
-          chmod u+rwx /home/yay
-
-
-              cd yay || { echo "Yay dizinine geçiş başarısız!"; exit 1; }
-              makepkg -si --noconfirm
-            
-                ;;
             "Anydesk")
                 echo "Anydesk kurulumu başlayacak..."
-                flatpak install anydesk -y
+                flatpak install anydesk -y || { echo "Anydesk kurulumu başarısız!"; exit 1; }
                 ;;
             "Whatsapp")
                 echo "Whatsapp kurulumu başlayacak..."
-                flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
+                flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y || { echo "Whatsapp kurulumu başarısız!"; exit 1; }
                 ;;
             "Discord")
                 echo "Discord kurulumu başlayacak..."
                 echo "$SUDO_PASSWORD" | sudo -S pacman -S discord --noconfirm
                 ;;
+
+            "ModluDc")
+                echo "Özel sürum discord indirmesi basliyor"
+                flatpak install vesktop -y || { echo "Vesktop kurulumu başarısız!"; exit 1; }
+                ;;
+            "Vscode")
+            echo "Visual Studio Code indirmesi baslatiliyor"
+            yay -S --noconfirm visual-studio-code-bin || { echo "Vscode kurulumu başarısız!"; exit 1; }
+            ;;
+            "JetbrainsTb")
+            echo "Jetbrains toolbox kurulumu basliyor"
+            yay -S --nofonfirm jetbrains-toolbox || { echo "Jetbrains kurulumu başarısız!"; exit 1; }
+            ;;
+
+
+
+
+
+
+
+
             *)
                 echo "Geçersiz seçim: $OPTION"
                 ;;
